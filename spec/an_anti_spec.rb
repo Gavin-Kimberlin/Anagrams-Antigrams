5efd121ac9d24202793a4109bacff5bbdcbfcb79
Gavin Kimberlin <guviner@gmail.com>
2019-07-19 10:02:12 -0700

    add fourth spec.

--- spec/an_anti_spec.rb
+++ spec/an_anti_spec.rb
@@ -10,6 +10,9 @@ describe('Anagram') do
   it('remnoves the first letter of a word and adds it to the end')
     expect(converter('hello')).to(eq('elloh'))
 
-  it('remnoves the second to last letter of a word and adds it to the middle')
+  it('removes the second to last letter of a word and adds it to the middle')
     expect(converter('elloh')).to(eq('elolh'))
+
+  it('swaps the first two letters of a word')
+    expect(converter('elolh')).to(eq('leolh'))
 end
