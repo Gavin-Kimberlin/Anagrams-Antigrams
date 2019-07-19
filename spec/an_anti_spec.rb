5ee8f27cced7d99716e893a7381d12a4c17240fb
Gavin Kimberlin <guviner@gmail.com>
2019-07-19 09:10:05 -0700

    add first spec.

--- spec/an_anti_spec.rb
+++ spec/an_anti_spec.rb
@@ -1,3 +1,8 @@
 require 'rspec'
 require 'allergies'
 require 'pry'
+
+describe('Anagram') do
+  it('checks to see if a word is an anagram')
+  expect(converter('elloh')).to(eq('this word is an anagram'))
+end
