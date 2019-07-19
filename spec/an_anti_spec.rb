480976b6f97b6e6b245220a2b33a36e79ce97598
Gavin Kimberlin <guviner@gmail.com>
2019-07-19 09:39:01 -0700

    add second spec.

--- spec/an_anti_spec.rb
+++ spec/an_anti_spec.rb
@@ -3,6 +3,10 @@ require 'allergies'
 require 'pry'
 
 describe('Anagram') do
-  it('checks to see if a word is an anagram')
-  expect(converter('elloh')).to(eq('this word is an anagram'))
+  it('checks to see if a word is an anagram') do
+    expect(converter('elloh')).to(eq('this word is an anagram'))
+  end
+
+  it('remnoves the first letter of a word and adds it to the end')
+    expect(converter('hello')).to(eq('elloh'))
 end
