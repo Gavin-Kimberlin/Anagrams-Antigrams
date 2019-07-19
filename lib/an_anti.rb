c73c369ab536ab494c8bf9b3e2fc0ba67646805e
Gavin Kimberlin <guviner@gmail.com>
2019-07-19 14:08:59 -0700

    wrote code for first spec.

--- lib/an_anti.rb
+++ lib/an_anti.rb
@@ -2,5 +2,10 @@ require 'pry'
 
 def converter()
   print "Enter a word:"
-  input = gets.chomp.split
+  input = gets.chomp.split("")
+
+  new_word = input[1,(input.length - 1)]
+  new_word.push(input[0])
+  # if input.length > 5
+  new_word.join(',')
 end
--- spec/an_anti_spec.rb
+++ spec/an_anti_spec.rb
@@ -7,7 +7,7 @@ describe('Anagram') do
     expect(converter('elloh')).to(eq('this word is an anagram'))
   end
 
-  it('remnoves the first letter of a word and adds it to the end')
+  it('removes the first letter of a word and adds it to the end')
     expect(converter('hello')).to(eq('elloh'))
 
   it('removes the second to last letter of a word and adds it to the middle')
